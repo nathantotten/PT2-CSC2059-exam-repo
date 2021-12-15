b10263578fde5977256f245eb83163d2ee0e9edb
nathantotten <74629300+nathantotten@users.noreply.github.com>
2021-12-15 15:03:14 +0000

    Finished 2 (c)

--- Project2/test2.cpp
+++ Project2/test2.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 // ========		(a)		========
@@ -79,7 +79,7 @@ int binarySearch(int* pi, int first, int last, int key)
 
 }
 
-int main()
+int main2()
 {
 	// B2. allocate an int array, pointed by pi, and store each number in an
 	// element of the array
@@ -109,5 +109,64 @@ int main()
 		cout << "The complexity of the binary search is O(logN)" << endl;
 	}
 	delete[] pi;
+	return 0;
+}
+
+
+// ========		(c)		========
+
+/* demonstration program for hashing-based search */
+// H1. write a good hash function (such as the one used in the lecture notes), which
+// converts a string key to a hash index in a hash table of the given tableSize
+size_t Hash_(string key, size_t tableSize)
+{
+	size_t index = 0;
+	for (int i = 0; i < key.size(); i++)
+		index = 37 * index + key[i];
+	return index % tableSize;
+}
+
+int main()
+{
+	// choose a suitable tableSize (e.g. 223), allocate an int array (i.e. the hash
+	// table) of this size pointed by pht
+	int tableSize = 223;
+	int* pht = new int[tableSize];
+	// H2. store the numbers in the hash table, e.g., 12 in a location specified by
+	// Hash("12", tableSize)
+	pht[Hash_("12", tableSize)] = 12;
+	pht[Hash_("211", tableSize)] = 211;
+	pht[Hash_("48", tableSize)] = 48;
+	pht[Hash_("501", tableSize)] = 501;
+	pht[Hash_("91", tableSize)] = 91;
+	pht[Hash_("82", tableSize)] = 82;
+	pht[Hash_("110", tableSize)] = 110;
+	pht[Hash_("4", tableSize)] = 4;
+
+	// run three search tests
+	for (int test = 1; test <= 3; test++) {
+		// take a search request from keyboard
+		cout << "Specify the number to be searched for: ";
+		int key;
+		cin >> key;
+		// H3. perform hashing-based search for the key number in the table;
+		// print the index if found or -1 otherwise
+		// need to make key into a string first so that we can put it into Hash_ function.
+		string sKey = to_string(key);
+		// get the hash index of the key (after converting to string for compatibility with Hash_)
+		int hashIndex = Hash_(sKey, tableSize);
+		// If the int at the index found using Hash_ in pht == key
+		if (int(pht[hashIndex]) == key)
+			// return the index
+			cout << hashIndex << endl;
+		else
+			// else return -1
+			cout << -1 << endl;
+
+		// H4. complete the statement, where XXX is what you think the
+		// complexity is in terms of a suitable measure
+		cout << "The hashing-based search is potentially O(1)" << endl;
+	}
+	delete[] pht;
 	return 0;
 }
