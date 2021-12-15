17b6d1c694fc52ec1063179c804438fa7ed5a6e9
nathantotten <74629300+nathantotten@users.noreply.github.com>
2021-12-15 14:27:33 +0000

    Completed 2 (b)

--- Project2/test2.cpp
+++ Project2/test2.cpp
@@ -2,8 +2,10 @@
 
 using namespace std;
 
+// ========		(a)		========
+
 /* demonstration program for linear search */
-int main()
+int main1()
 {
 	// L1. allocate an int array, pointed by pi, and store each number in an
 	// element of the array (e.g., pi[0] = 12, pi[1] = 211, ..., pi[7] = 4)
@@ -41,5 +43,71 @@ int main()
 	// L4. free the memory used by the array
 	delete[] pi;
 
+	return 0;
+}
+
+// ========		(b)		========
+
+/* demonstration program for binary search */
+// B1. implement binary search, below, through an int array pointed by pi from first
+// to last for a specified key number; return the index if found or -1 otherwise
+int binarySearch(int* pi, int first, int last, int key)
+{
+	// base case - not found
+	if (first > last) return -1;
+
+	// recursive case
+
+	//midpoint
+	
+	int mid = (first + last) / 2;
+
+	
+	// get the integer at the midpoint
+	int val = pi[mid];
+	
+	// if the value is equal to the key then return the index (midpoint)
+	if (val == key)
+		return mid;
+	// else if the key is smaller than the value at the midpoint, navigate
+	//	to the left subtree
+	else if (key < val)
+		return binarySearch(pi, first, mid - 1, key);
+	else
+		// else it must be in the right subtree
+		return binarySearch(pi, mid + 1, last, key);
+
+}
+
+int main()
+{
+	// B2. allocate an int array, pointed by pi, and store each number in an
+	// element of the array
+
+	int* pi = new int[8];
+	pi[0] = 4;
+	pi[1] = 12; 
+	pi[2] = 48;
+	pi[3] = 82;
+	pi[4] = 91;
+	pi[5] = 110;
+	pi[6] = 211;
+	pi[7] = 501;
+
+	// run three search tests
+	for (int test = 1; test <= 3; test++) {
+		// take a search request from keyboard
+		cout << "Specify the number to be searched for: ";
+		int key;
+		cin >> key;
+		// B3. call binarySearch to search for the key number; print the
+		// index if found or -1 otherwise
+		
+		cout << binarySearch(pi, 0, 7, key) << endl;
+		// B4. complete the statement, where XXX is what you think the
+		// complexity is in terms of the order(s) of the size of the data set
+		cout << "The complexity of the binary search is O(logN)" << endl;
+	}
+	delete[] pi;
 	return 0;
 }
